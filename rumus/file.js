e95e8fb4b4de065d4de02e7722c0a9e70c19cf92
rendyar <rendyafriatama11@gmail.com>
2021-08-19 12:06:14 +0700

    commit

---
+++ rumus/file.js
@@ -0,0 +1,32 @@
+function persegi() {
+    var s1 = prompt("Masukkan sisi")
+    var s2 = prompt("Masukkan sisi")
+    var hasilp = s1*s2;
+
+    alert(hasilp);
+}
+
+function persegipanjang() {
+    var p = prompt("Masukkan panjang")
+    var l = prompt("Masukkan lebar")
+    var hasilpp = p*l;
+
+    alert(hasilpp);
+}
+
+function segitiga() {
+    const s = 0.5
+    var a = prompt("Masukkan alas")
+    var t = prompt("Masukkan tinggi")
+    var hasils = a*t*s;
+
+    alert(hasils);
+}
+
+function jajargenjang() {
+    var a = prompt("Masukkan alas")
+    var t = prompt("Masukkan tinggi")
+    var hasilj = a*t;
+
+    alert(hasilj);
+}
